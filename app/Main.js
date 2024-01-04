9436d04391d0bf558f3a4f993b47289a68edd7af
erick salazar <ericksalazar_g@hotmail.com>
2024-01-04 12:26:13 -0600

    adding my new react webapp

---
+++ app/Main.js
@@ -0,0 +1,25 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+//My Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+function Main() {
+  return (
+    <>
+      <Header />
+      <Terms />
+      <Footer />
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<Main />)
+
+if (module.hot) {
+  module.hot.accept()
+}
