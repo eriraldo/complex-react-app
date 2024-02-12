3a32e63a178ea0d40e2205d2e02cda58dc7352b8
Erick <ericksalazar_g@hotmail.com>
2024-02-12 08:43:47 -0600

    adding home page to the project and creating validation to swap between loggedin and main pages so that the information displayed correctly

--- app/Main.js
+++ app/Main.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import Home from "./components/Home"
 //My Components
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
@@ -9,22 +9,26 @@ import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
 function Main() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomeGuest />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/terms" element={<Terms />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  )
+    const [loggedIn, setLoggedIn] = useState(
+        Boolean(localStorage.getItem("complexappToken"))
+    )
+
+    return (
+        <BrowserRouter>
+            <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+            <Routes>
+                <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/terms" element={<Terms />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 const root = ReactDOM.createRoot(document.querySelector("#app"))
 root.render(<Main />)
 
 if (module.hot) {
-  module.hot.accept()
+    module.hot.accept()
 }
